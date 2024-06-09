860e9454e0c94bfaf3aeb50973835cb9ab829ec7
Quan Cao <84135123+qcao-expr@users.noreply.github.com>
2024-06-08 22:43:06 -0400

    sockets working

--- apps/pobuto-poker/client/src/App.tsx
+++ apps/pobuto-poker/client/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface ServerToClientEvents {
+  message: (message: string) => void;
 }
 
-export default App
+export const App = () => {
+  const [message, setMessage] = useState<string>('Connecting to Socket IO..');
+
+  useEffect(() => {
+    const socket: Socket<ServerToClientEvents> = io('http://localhost:4000', {
+      transports: ['websocket'],
+    });
+
+    socket.on('message', (message) => {
+      setMessage(message);
+    });
+
+    socket.on('connect_error', (err) => {
+      console.error(err);
+      setMessage('Failed to connect to Socket IO');
+    });
+  }, []);
+
+  return <p>{message}</p>;
+};
--- apps/pobuto-poker/server/src/main.ts
+++ apps/pobuto-poker/server/src/main.ts
@@ -1,5 +1,31 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `${__dirname}/../.env.development` });
+const { HOST_URL, SERVER_PORT, CLIENT_URL } = process.env;
 
 const app = express();
 
-app.listen(4001);
+app.use(cors({ origin: CLIENT_URL }));
+
+app.get('/env', (_, res) => {
+  res.send({ HOST_URL, SERVER_PORT });
+});
+
+const server = app.listen(SERVER_PORT, () => {
+  console.log(`Pobuto Poker server started on port :${SERVER_PORT}`);
+});
+
+const socketio = new Server(server);
+socketio.on('connection', (socket) => {
+  console.log('SocketIO connected.');
+
+  setInterval(() => {
+    socket.emit(
+      'message',
+      `Hello from Socket IO! Heres a random number: ${Math.random()}!`
+    );
+  }, 2000);
+});
